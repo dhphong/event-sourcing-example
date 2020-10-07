97dfb5b66a49ab1f9602926a11f8b5c2c011796c
Do Phong <phong987phong@gmail.com>
2020-10-07 12:20:22 +0700

    first commit

---
+++ src/test/java/edu/dhphong/eventsourcingexample/EventSourcingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dhphong.eventsourcingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
